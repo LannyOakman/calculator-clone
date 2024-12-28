ab88789fb8ea6e010fba324186c27a29be9bb04e
LannyOakman <lannyoakman@gmail.com>
2024-12-27 23:35:52 -0500

    finishing general gui.

--- ios_calculator_clone/lib/main.dart
+++ ios_calculator_clone/lib/main.dart
@@ -10,26 +10,72 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.95,
-          margin: EdgeInsets.only(top: 390),
-          
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children:[
-              for(var items in CalculatorLayout.calculator_layout) Column(
-                  children: <FormattedButton>[
-                    for(var item in items) FormattedButton(color: item.color, icon: item.icon, string: item.string,)
-                  ],
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            Spacer(),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 40,
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: EdgeInsets.only(right: 5),
+                  child: Text(
+                    "123,123,123",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
-            ],
-          ),
-      )),
-    ));
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 90,
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: EdgeInsets.only(right: 5),
+                  child: Text(
+                    "123,123,123",
+                    style: TextStyle(
+                      fontSize: 60,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 1,
+              height: MediaQuery.of(context).size.height * 0.615,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  for (var items in CalculatorLayout.calculator_layout)
+                    Column(
+                      children: <FormattedButton>[
+                        for (var item in items)
+                          FormattedButton(
+                            color: item.color,
+                            icon: item.icon,
+                            string: item.string,
+                          )
+                      ],
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

1788979df4925db2ed364a37a8162528b0ae4b06
LannyOakman <lannyboy2009@gmail.com>
2025-01-01 20:01:18 -0500

    moving logic outside of main

--- ios_calculator_clone/lib/main.dart
+++ ios_calculator_clone/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ios_calculator_clone/data/calculator_layout.dart';
-import 'package:ios_calculator_clone/widgets/formatted_button.dart';
+import 'package:ios_calculator_clone/widgets/calculator.dart';
 
 void main() {
   runApp(const MainApp());
@@ -16,66 +15,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Spacer(),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 40,
-              child: Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: EdgeInsets.only(right: 5),
-                  child: Text(
-                    "123,123,123",
-                    style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 90,
-              child: Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: EdgeInsets.only(right: 5),
-                  child: Text(
-                    "123,123,123",
-                    style: TextStyle(
-                      fontSize: 60,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 1,
-              height: MediaQuery.of(context).size.height * 0.615,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  for (var items in CalculatorLayout.calculator_layout)
-                    Column(
-                      children: <FormattedButton>[
-                        for (var item in items)
-                          FormattedButton(
-                            color: item.color,
-                            icon: item.icon,
-                            string: item.string,
-                            num: item.num,
-                          )
-                      ],
-                    ),
-                ],
-              ),
-            ),
-          ],
-        ),
+        body: Calculator()
       ),
     );
   }
